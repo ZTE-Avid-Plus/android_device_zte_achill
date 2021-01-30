def07a33965d475a2709ee84381450f0158bb293
alexenferman <alextud0132@gmail.com>
2021-01-29 19:23:19 -0500

    msm8909 -> achill

--- init/Android.mk
+++ init/Android.mk
@@ -7,7 +7,7 @@ LOCAL_C_INCLUDES := \
     system/core/base/include \
     system/core/init
 LOCAL_CFLAGS := -Wall
-LOCAL_SRC_FILES := init_msm8909.cpp
+LOCAL_SRC_FILES := init_achill.cpp
 ifneq ($(TARGET_LIBINIT_MSM8909_DEFINES_FILE),)
   LOCAL_SRC_FILES += ../../../../$(TARGET_LIBINIT_MSM8909_DEFINES_FILE)
 endif
